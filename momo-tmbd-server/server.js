7b3560af018ae822e51ef7aaaa2a5be4567272a6
Arjun-707 <ajunakki707@gmail.com>
2021-06-27 15:14:15 +0530

    removed

--- momo-tmbd-server/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { initMiddleware, routeMiddleware } = require("./middleware");
-const app = express();
-initMiddleware(app);
-routeMiddleware(app);
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, (_) => console.log(`APP is listening at PORT: ${PORT}`));
